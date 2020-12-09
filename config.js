5c49475f6a0b1f3c51a71789d313bce22e674eee
Amchelle Clendenin <amchelle.clendenin@utexas.edu>
2020-12-09 12:59:52 -0600

    changes to config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/koaCapital/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Koa Capital',
   subHeading: 'Proprietary Trading Firm',
 
